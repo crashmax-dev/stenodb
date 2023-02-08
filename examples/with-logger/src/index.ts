81656bfb8d1b5d2b65414da0f5305208f24d4b37
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-02-08 21:49:01 +0800

    feat(examples): add `with-logger`

---
+++ examples/with-logger/src/index.ts
@@ -0,0 +1,72 @@
+import 'reflect-metadata'
+import { el } from '@zero-dependency/dom'
+import { User } from './entities.js'
+import { storage } from './storage.js'
+
+const app = document.querySelector('#app')!
+
+const userIdInput = el('input', {
+  type: 'number',
+  name: 'id',
+  disabled: true
+})
+
+const usernameInput = el('input', {
+  type: 'text',
+  name: 'username',
+  placeholder: 'Username'
+})
+
+const submitButton = el(
+  'button',
+  {
+    type: 'submit'
+  },
+  'Submit'
+)
+
+const resetButton = el(
+  'button',
+  {
+    onclick: () => {
+      storage.reset()
+      render()
+    }
+  },
+  'Reset'
+)
+
+const form = el(
+  'form',
+  {
+    onsubmit: (event: Event) => {
+      event.preventDefault()
+      const formData = new FormData(form)
+      const username = formData.get('username')?.toString()
+      if (!username) {
+        usernameInput.focus()
+        return
+      }
+      const user = new User(storage.data!.getLastUserId() + 1, username)
+      storage.data!.addUser(user)
+      storage.write()
+      render()
+    }
+  },
+  userIdInput,
+  usernameInput,
+  submitButton,
+  resetButton
+)
+
+const storagePreview = el('pre')
+
+function render() {
+  form.reset()
+  usernameInput.focus()
+  userIdInput.value = storage.data!.getLastUserId().toString()
+  storagePreview.textContent = JSON.stringify(storage.data, null, 2)
+}
+
+render()
+app.append(form, storagePreview)

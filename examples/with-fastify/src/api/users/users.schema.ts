ca72707f81b3ccf062f21e93a9b0de6042792d30
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-03-15 15:41:27 +0800

    fix(examples/with-fastify): params userId schema

--- examples/with-fastify/src/api/users/users.schema.ts
+++ examples/with-fastify/src/api/users/users.schema.ts
@@ -1,8 +1,13 @@
+import { userIdParamsSchema } from '../posts/posts.schema.js'
+
 export const userBodySchema = {
   body: { $ref: 'User' }
 }
 
 export const userResponseSchema = {
+  params: {
+    ...userIdParamsSchema.params
+  },
   response: {
     200: { $ref: 'User' }
   }
